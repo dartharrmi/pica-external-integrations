c3c6a4943c1d69a74def966ce772ea1b888bc8de
Miguel Arroyo <jmiguelarroyo12@gmail.com>
2019-05-28 21:25:00 -0500

    Mocks response for Hilton rooms

--- restwrapper-airlines/src/main/java/com/archilabs/pica/restwrapper/hilton/room/service/HiltonRoomsService.java
+++ restwrapper-airlines/src/main/java/com/archilabs/pica/restwrapper/hilton/room/service/HiltonRoomsService.java
@@ -11,6 +11,7 @@ import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 import java.math.BigInteger;
 import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.GregorianCalendar;
 import java.util.List;
 
@@ -33,6 +34,71 @@ public class HiltonRoomsService implements IHiltonRoomsService {
         request.setType(roomsRequest.type);
         request.setRooms(BigInteger.valueOf(roomsRequest.rooms));
 
-        return hiltonRoomsClient.getHiltonRooms(request).getResult().getRooms();
+        //return hiltonRoomsClient.getHiltonRooms(request).getResult().getRooms();
+        List<Room> rooms = new ArrayList<>();
+
+        Room r1 = new Room();
+        r1.setNumber(new BigInteger("123456"));
+        r1.setPrice(250000);
+        r1.setType("Double");
+
+        Room r2 = new Room();
+        r2.setNumber(new BigInteger("123457"));
+        r2.setPrice(350000);
+        r2.setType("Double Premium");
+
+        Room r3 = new Room();
+        r3.setNumber(new BigInteger("123458"));
+        r3.setPrice(250000);
+        r3.setType("Double");
+
+        Room r4 = new Room();
+        r4.setNumber(new BigInteger("123459"));
+        r4.setPrice(260000);
+        r4.setType("Double");
+
+        Room r5 = new Room();
+        r5.setNumber(new BigInteger("123460"));
+        r5.setPrice(240000);
+        r5.setType("Double");
+
+        Room r6 = new Room();
+        r6.setNumber(new BigInteger("123461"));
+        r6.setPrice(120000);
+        r6.setType("Single");
+
+        Room r7 = new Room();
+        r7.setNumber(new BigInteger("123462"));
+        r7.setPrice(650000);
+        r7.setType("Suite");
+
+        Room r8 = new Room();
+        r8.setNumber(new BigInteger("123463"));
+        r8.setPrice(550000);
+        r8.setType("Suite Economy");
+
+        Room r9 = new Room();
+        r9.setNumber(new BigInteger("123464"));
+        r9.setPrice(100000);
+        r9.setType("Single");
+
+        Room r10 = new Room();
+        r10.setNumber(new BigInteger("123465"));
+        r10.setPrice(380000);
+        r10.setType("Double Premium");
+
+
+        rooms.add(r1);
+        rooms.add(r2);
+        rooms.add(r3);
+        rooms.add(r4);
+        rooms.add(r5);
+        rooms.add(r6);
+        rooms.add(r7);
+        rooms.add(r8);
+        rooms.add(r9);
+        rooms.add(r10);
+
+        return rooms;
     }
 }
